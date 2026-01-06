e2e749db08432cbf854e85e4ff0c58da2c555125
FaustinBushenyula <fbushenyula@gmail.com>
2026-01-06 15:16:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Menu responsive
+const toggleBtn = document.getElementById("menu-toggle");
+const menu = document.querySelector(".menu");
+
+toggleBtn.addEventListener("click", () => {
+  menu.classList.toggle("show");
+});
+
+// Message de bienvenue
+//window.addEventListener("load", () => {
+  //alert("Bienvenue sur mon site web !");
+//});
